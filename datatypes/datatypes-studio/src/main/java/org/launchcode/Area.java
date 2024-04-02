76db9aac1aa5b8c598daa440fcdaf0fa30feefe0
jdcheek <58921502+jdcheek@users.noreply.github.com>
2024-04-01 20:09:23 -0500

    chapter 2 studio

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,15 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the radius of the circle: ");
+
+        double radius = scanner.nextDouble();
+        double area = Circle.getArea(radius);
+
+        System.out.println("The area of the circle with radius " + radius + " is: " + area);
+    }
+}
